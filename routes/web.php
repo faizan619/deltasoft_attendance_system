956b863697870cf073eba535ebbe6369ed49a8f0
faizan alam <alamf6023@gmail.com>
2025-03-18 16:01:55 +0530

    routes done

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,18 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+Route::redirect('/','/login');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Auth Routes
+Route::get('/login',[UserController::class,'adminLogin'])->name('adminLogin');
+Route::post('/login-check',[UserController::class,'LoginProceed'])->name('LoginProceed');
+Route::post('/logout', [UserController::class, 'logout'])->name('logout');
 
-Route::get('/login',function(){
-    return view('auth.login');
-});
-
-Route::get('/hmm',function(){
-    return view('hello');
-});
+// Dashboard Routes
+Route::get('/dashboard',function(){
+    return view('dashboard');
+})->name("admin_dashboard");

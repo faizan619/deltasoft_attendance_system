be9e0f61b6e2d31ae7887ae94d167b6ddc1482a6
faizan alam <alamf6023@gmail.com>
2025-03-19 11:43:44 +0530

    header n footer done

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="mt-auto py-4 px-3 bg-dark text-light">
-    <div class="d-flex justify-content-center align-items-center">
-        <b>Design and Develop By DeltaSoft Systems</b>
+    <div class="d-flex justify-content-center align-items-center text-center">
+        <b>Design and Develop By <a class="text-white" target="_faizan" href="https://www.deltasoftsys.in/">DeltaSoft Systems</a></b>
     </div>
 </div>
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -10,24 +10,24 @@
                     <a class="nav-link active" aria-current="page" href="{{ route('admin_dashboard') }}">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" target="_faizan" href="https://www.deltasoftsys.in/">Official Page</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
+                        Services
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" target="_faizan" href="https://www.deltasoftsys.in/website-design">WebSite Design</a></li>
+                        <li><a class="dropdown-item" target="_faizan" href="https://www.deltasoftsys.in/website-development">Website Development</a></li>
                         <li>
                             <hr class="dropdown-divider">
                         </li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <li><a class="dropdown-item" target="_faizan" href="https://www.deltasoftsys.in/application-development">Application Development</a></li>
                     </ul>
                 </li>
-                <li class="nav-item">
+                <!-- <li class="nav-item">
                     <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                </li>
+                </li> -->
             </ul>
             <div class="d-flex">
                 <form action="{{ route('logout')}}" method="POST">

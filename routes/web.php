6b0fc9f9535565a977a1171d0d19bb50aa5daa50
faizan alam <alamf6023@gmail.com>
2025-04-09 11:34:57 +0530

    routes created

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AttendanceController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 Route::redirect('/','/login');
@@ -15,4 +16,6 @@ Route::post('/logout', [UserController::class, 'logout'])->name('logout');
 // Dashboard Routes
 Route::get('/dashboard',[UserController::class,'admin_dashboard'])->name("admin_dashboard");
 
-Route::get('/user',[UserController::class,'user_dashboard'])->name('user_dashboard'); 
+Route::get('/user',[AttendanceController::class,'user_dashboard'])->name('user_dashboard'); 
+Route::post('/user-attendance',[AttendanceController::class,'store'])->name('attendance.store');
+Route::get('/user-reached',[AttendanceController::class,'reached'])->name('reached');

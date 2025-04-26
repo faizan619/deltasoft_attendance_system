fa239909e873047641a78869f692e316faea1932
faizan alam <alamf6023@gmail.com>
2025-04-26 15:27:08 +0530

    routes updated

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,12 @@ Route::post('/logout', [UserController::class, 'logout'])->name('logout');
 // Dashboard Routes
 Route::get('/dashboard',[UserController::class,'admin_dashboard'])->name("admin_dashboard");
 
+// Admin Part
+Route::get('/add-employee',[UserController::class,'add_employee'])->name("add_employee");
+Route::post('/save-employee',[UserController::class,'save_employee'])->name("save_employee");
+Route::get('/emp-list',[UserController::class,'ViewEmpList'])->name('emp_list');
+
 Route::get('/user',[AttendanceController::class,'user_dashboard'])->name('user_dashboard'); 
 Route::post('/user-attendance',[AttendanceController::class,'store'])->name('attendance.store');
+Route::get('/user-reason',[AttendanceController::class,'attendanceReason'])->name('attendanceReason');
 Route::get('/user-reached',[AttendanceController::class,'reached'])->name('reached');

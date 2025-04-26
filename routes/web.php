761a477a79166402c7492acc485a6aeb59be7e0a
faizan alam <alamf6023@gmail.com>
2025-04-26 16:06:07 +0530

    routes modifed

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\AttendanceController;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\checkAdmin;
+use App\Http\Middleware\checkUser;
 use Illuminate\Support\Facades\Route;
 Route::redirect('/','/login');
 // Route::get('/', function () {
@@ -17,11 +19,11 @@ Route::post('/logout', [UserController::class, 'logout'])->name('logout');
 Route::get('/dashboard',[UserController::class,'admin_dashboard'])->name("admin_dashboard");
 
 // Admin Part
-Route::get('/add-employee',[UserController::class,'add_employee'])->name("add_employee");
-Route::post('/save-employee',[UserController::class,'save_employee'])->name("save_employee");
-Route::get('/emp-list',[UserController::class,'ViewEmpList'])->name('emp_list');
+Route::get('/add-employee',[UserController::class,'add_employee'])->name("add_employee")->middleware([checkAdmin::class]);
+Route::post('/save-employee',[UserController::class,'save_employee'])->name("save_employee")->middleware([checkAdmin::class]);
+Route::get('/emp-list',[UserController::class,'ViewEmpList'])->name('emp_list')->middleware([checkAdmin::class]);
 
-Route::get('/user',[AttendanceController::class,'user_dashboard'])->name('user_dashboard'); 
-Route::post('/user-attendance',[AttendanceController::class,'store'])->name('attendance.store');
-Route::get('/user-reason',[AttendanceController::class,'attendanceReason'])->name('attendanceReason');
-Route::get('/user-reached',[AttendanceController::class,'reached'])->name('reached');
+Route::get('/user',[AttendanceController::class,'user_dashboard'])->name('user_dashboard');     // Role is Already Checking in Controller so no need of middleware here
+Route::post('/user-attendance',[AttendanceController::class,'store'])->name('attendance.store')->middleware([checkUser::class]);
+Route::get('/user-reason',[AttendanceController::class,'attendanceReason'])->name('attendanceReason')->middleware([checkUser::class]);
+Route::get('/user-reached',[AttendanceController::class,'reached'])->name('reached')->middleware([checkUser::class]);

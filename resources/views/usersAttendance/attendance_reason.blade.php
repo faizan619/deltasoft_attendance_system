e32b8d8d5a53fc94579da5919c9e98e09ea19eec
faizan alam <alamf6023@gmail.com>
2025-06-10 12:41:43 +0530

    resources modified

--- resources/views/usersAttendance/attendance_reason.blade.php
+++ resources/views/usersAttendance/attendance_reason.blade.php
@@ -2,8 +2,6 @@
 
 @section('content')
 <div class="container pt-5">
-    <!-- <div class="">Hello Give me Your Reason.</div> -->
-    <!-- <div class="">You Reach at {{\Carbon\Carbon::parse($attendanceTime)->format('g:i A')}}</div> -->
     <div class="row">
         <div class="col-md-8 mx-auto">
             <div class="card">
@@ -11,16 +9,36 @@
                 <div class="card-body text-center">
                     <h3 class="mb-3">{{$note}}</h3>
                     <!-- <h4></h4> -->
-                    <h5> @if ($title == "Checkin")
-                        You'r Entry Timing is {{\Carbon\Carbon::parse($empCheck->entryTime)->format('g:i A')}}
+                    <h5>
+
+                    
+                        
+                        @if ($title == "Checkin")
+                            You'r Entry Timing is {{ \Carbon\Carbon::parse($empCheck->entryTime)->format('g:i A') }}
+                            and You'r doing {{ $title }} at 
+                            @if($attendanceTime) 
+                                {{ \Carbon\Carbon::parse($attendanceTime)->format('g:i A') }} 
+                            @elseif ($attendanceCheckoutTime) 
+                                {{ \Carbon\Carbon::parse($attendanceCheckoutTime)->format('g:i A') }} 
+                            @endif
+
                         @elseif ($title == "Checkout")
-                        You'r Leaving Timing is {{\Carbon\Carbon::parse($empCheck->exitTime)->format('g:i A')}}
-                        @endif and You'r doing {{$title}} at {{\Carbon\Carbon::parse($attendanceTime)->format('g:i A')}}</h5>
+                            You'r Leaving Timing is {{ \Carbon\Carbon::parse($empCheck->exitTime)->format('g:i A') }}
+                            and You'r doing {{ $title }} at 
+                            @if(isset($attendanceTime) && $attendanceTime) 
+                                {{ \Carbon\Carbon::parse($attendanceTime)->format('g:i A') }} 
+                            @elseif(isset($attendanceCheckoutTime) && $attendanceCheckoutTime) 
+                                {{ \Carbon\Carbon::parse($attendanceCheckoutTime)->format('g:i A') }} 
+                            @endif
+                        @endif
+
+                        
+                    </h5>
                     <form method="post" action="{{ route('attendance.store') }}" class="row mt-3">
                         @csrf
-                        @if($attendanceTime)
+                        @if(isset($attendanceTime) && $attendanceTime)
                             <input type="datetime-local" name="attendance_time" id="attendance_time" value="{{$attendanceTime}}" style="display: none;">
-                        @elseif ($attendanceCheckoutTime)
+                        @elseif (isset($attendanceCheckoutTime) && $attendanceCheckoutTime)
                             <input type="datetime-local" name="attendanceCheckoutTime" id="attendanceCheckoutTime" value="{{$attendanceCheckoutTime}}" style="display: none;">
                         @endif
                         <div class="col-md-8">
